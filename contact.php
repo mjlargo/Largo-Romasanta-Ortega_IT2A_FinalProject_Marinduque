8fbd7b156643dd281a26626fb6e4843131970556
mjlargo <mjlargo015@gmail.com>
2023-05-29 14:52:55 +0800

    XML document added

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@ if(!file_exists('users/' . $_SESSION['username'] . '.xml')) {
 
 <head>
     <meta charset="utf-8">
-    <title><?php echo "Restoran - Bootstrap Restaurant Template"?></title>
+    <title>Contact Us</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
--- reviews.php
+++ reviews.php
@@ -9,7 +9,7 @@ $img4 = "img/testimonial-4.jpg";
 
 <head>
     <meta charset="utf-8">
-    <title>Marinduque <?php echo "Bootstrap Restaurant Template"?></title>
+    <title>Reviews</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
